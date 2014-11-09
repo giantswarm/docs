2284aaae71ddc2f62d47c255ef65481755890914
marian <marian@giantswarm.io>
2014-11-09 17:38:06 +0100

    Detach indexer from console

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # start indexing content for search
-#cd search && python ./searchindexer.py &
-cd search && python indexer.py
+cd search && python indexer.py &
 
 # use mkdocs server to publish pages
 cd /docs && exec mkdocs serve
