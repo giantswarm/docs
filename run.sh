ea906a18093dcc4505fd2c6cf8e1db50ec5ea373
marian <marian@giantswarm.io>
2015-01-30 17:51:21 +0100

    Added commenting, added wait before indexing, added DEVELOPMENT_MODE

--- run.sh
+++ run.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
+# This script runs the content server in production.
+# If a link to the search service exists, it runs
+# the search indexer before starting the server, to
+# push new content to the search engine.
+
+# Run indexer (if ElasticSearch address is configured)
 [ -z "$SITESEARCH_PORT_9200_TCP_ADDR" ] || {
 	echo "SITESEARCH_PORT_9200_TCP_ADDR is set. Indexing.";
 	# set SITESEARCH ip/port for search indexer
 	echo "- ${SITESEARCH_PORT_9200_TCP_ADDR}:${SITESEARCH_PORT_9200_TCP_PORT}" > search/hosts.yml;
 	# start indexing content for search
-	cd search && python indexer.py &
+	sleep 10
+	cd /docs/search && python ./indexer.py
 }
 
-# use mkdocs server to publish pages
-cd /docs && exec mkdocs serve
+
+cd /docs/swarmdocs
+
+if [ -z ${DEVELOPMENT_MODE+x} ];
+then
+	exec hugo server -p 8000
+else 
+	exec hugo server -p 8000 -w
+fi
