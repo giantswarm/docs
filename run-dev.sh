26d8bdb85ca595041c373726d26fa2b96314a7a7
marians <marian@giantswarm.io>
2015-04-15 07:39:22 +0200

    Modified run-script for development

--- run-dev.sh
+++ run-dev.sh
@@ -7,5 +7,8 @@ rm -rf swarmdocs/content
 rm -rf swarmdocs/static/img
 cp -r docs-content/content swarmdocs/
 cp -r docs-content/img swarmdocs/static/
+mkdir -p swarmdocs/content/api
+cat api/frontmatter.txt > swarmdocs/content/api/methods.md
+cat ../api/docs/api.md >> swarmdocs/content/api/methods.md
 
 cd swarmdocs && hugo server -ws .
