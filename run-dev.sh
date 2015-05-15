17e3400936cb07b86d8d667901cdfcd1777a9c9b
Kord Campbell <kordless@gmail.com>
2015-05-15 15:03:48 -0700

    rolling marians changes from may-iteration-2nd in

--- run-dev.sh
+++ run-dev.sh
@@ -5,7 +5,7 @@ echo `date +"%Y%m%d%H%M"` > swarmdocs/layouts/partials/cache_datestamp.html
 rm -rf swarmdocs/public/*
 rm -rf swarmdocs/content
 rm -rf swarmdocs/static/img
-cp -r docs-content/content swarmdocs/
-cp -r docs-content/img swarmdocs/static/
+cp -r content swarmdocs/
+cp -r img swarmdocs/static/
 
 cd swarmdocs && hugo server -ws .
