c09107b4c4127a7a91c7567cd66547234663dfb1
Kord Campbell <kordless@gmail.com>
2015-08-06 18:14:32 -0700

    do not delete, just copy

--- reload.sh
+++ reload.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-echo "rebuilding..."
-rm -rf swarmdocs/public/*
-rm -rf swarmdocs/content
-rm -rf swarmdocs/static/img
+echo "updating..."
 cp -r docs-content/content swarmdocs/
 cp -r docs-content/img swarmdocs/static/
 sleep 60
