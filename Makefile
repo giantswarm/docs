2c4381ac4b2c5b3f63411ef84fc1b121c7cd8301
Oliver Ponder <oliver.ponder@gmail.com>
2017-03-22 06:04:25 +0100

    Add repeatable task for vendorizing external repositories

--- Makefile
+++ Makefile
@@ -9,42 +9,44 @@ build-css:
 	sass src/static/css/base.sass src/static/css/base.css
 
 vendor: clean
+	# Vendor docs-content
 	mkdir vendor
 	git clone --depth 1 git@github.com:giantswarm/docs-content.git vendor/docs-content
-	rm -rf vendor/docs-content/.git
+	rm -rf vendor/docs-content/.git # Ensure git doesn't commit this as a subproject, but as actual files in the repo
 
+	# Vendor hugo
 	mkdir vendor/hugo
 	cd vendor/hugo && wget https://github.com/spf13/hugo/releases/download/v0.16/hugo_0.16_linux-64bit.tgz
 	cd vendor/hugo && tar -xvf hugo_0.16_linux-64bit.tgz
 	cd vendor/hugo && rm hugo_0.16_linux-64bit.tgz
 
+	# Vendor other external repositories as defined in docs-content repo 'external-repositories.txt'
+	./vendorize-external-repositories.sh
+
 build: build-css
-	#
-	# clean
+	# Clean
 	rm -rf build
 
-	# create build directory
+	# Create build directory
 	mkdir build
 
-	# copy src to build directory
+	# Copy src to build directory
 	cp -r src/. build/
-	#
-	# copy content from docs-content repo
+
+	# Copy content from docs-content repo
 	cp -r vendor/docs-content/content build/content
 	cp -r vendor/docs-content/img build/static/
-	#
+
 	# Cache breaker
 	echo -n `git hash-object ./build/static/css/base.css|head -c 9` > build/layouts/partials/cachebreaker_css.html
 	echo -n `git hash-object ./build/static/js/base.js|head -c 9` > build/layouts/partials/cachebreaker_js.html
-	#
+
 	# Latest gsctl version
 	mkdir -p build/layouts/shortcodes
 	curl -s https://downloads.giantswarm.io/gsctl/VERSION > build/layouts/shortcodes/gsctl_version.html
 
-	#
-	# tie in recipes frome external repositories
+	# Tie in content from external repositories
 	./build-external-repositories.sh
-	# build docker image
 
 docker-build: build
 	docker build -t $(registry)/$(COMPANY)/$(PROJECT) .
--- vendorize-external-repositories.sh
+++ vendorize-external-repositories.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Here we pull in content from external repositories. While doing so we do
 # - copy images (png, jpg)to a special folder
@@ -14,16 +14,7 @@
 # - There may be additional markdown files in that folder
 # - There may be PNG and/or JPG images in that folder
 
-# Path to where recipe content should be placed in the docs site
-CONTENT_BASE_DIR=$(pwd)/src
-
-# Path to where images should be copied to
-IMG_BASE_DIR=$(pwd)/src/static/img
-
-mkdir -p build
-cd build
-
-cat ../vendor/docs-content/external-repositories.txt | while read repoline
+cat ./vendor/docs-content/external-repositories.txt | while read repoline
 do
 
   # split line into repository URL, target path
@@ -31,7 +22,7 @@ do
   repourl=${parts[0]}
   targetpath=${parts[1]}
 
-  echo "\nImporting content from ${repourl} to ${targetpath}"
+  echo "\Vendorizing ${repourl}"
 
   # derive reponame from repourl
   filename=$(basename ${repourl})
@@ -42,23 +33,8 @@ do
   rm -rf ${reponame}
 
   # Clone the repository from github
-  git clone --depth 1 https://github.com/giantswarm/${reponame}.git
+  git clone --depth 1 https://github.com/giantswarm/${reponame}.git vendor/${reponame}
 
-  # Create folders for markdown and images
-  mkdir -p ${CONTENT_BASE_DIR}/${targetpath}/${reponame}
-  mkdir -p ${IMG_BASE_DIR}/${reponame}
-
-  # Copy repo content to target location
-  cp ${reponame}/docs/*.md ${CONTENT_BASE_DIR}/${targetpath}/${reponame}/
-  cp ${reponame}/docs/*.png ${IMG_BASE_DIR}/${reponame}/
-  cp ${reponame}/docs/*.jpg ${IMG_BASE_DIR}/${reponame}/
-
-  # adapt image paths
-  for markdownfile in ${CONTENT_BASE_DIR}/${targetpath}/${reponame}/*.md; do
-    sed -i ".bak" -E "s:\(([^\(]+\.)(png|jpg|jpeg|gif):\(/img/${reponame}/\1\2:g" "${markdownfile}"
-    rm -f "${markdownfile}.bak"
-  done
-
-  echo "<hr>\n" >> ${CONTENT_BASE_DIR}/${targetpath}/${reponame}/index.md
-  echo "You can collaborate on this recipe on [GitHub](https://github.com/giantswarm/${reponame})." >> ${CONTENT_BASE_DIR}/${targetpath}/${reponame}/index.md
+  # Delete .git folder so that git doesn't commit this as a subproject, but as actual files in the repo
+  rm -rf vendor/${reponame}/.git
 done
