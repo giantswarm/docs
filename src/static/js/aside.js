c7bbd53b0fdbe5401ac94f91833ba095020e3b95
axbarsan <mnlk123@yahoo.com>
2020-05-12 15:53:46 +0200

    Actually store the aside as a global variable

--- src/static/js/aside.js
+++ src/static/js/aside.js
@@ -1,51 +1,49 @@
-(function() {
-  function GSAside(asideSelector, contentSelector, topOffset) {
-    if (!asideSelector || !contentSelector) {
-      throw new Error('Both selectors cannot be null.');
-    }
-
-    this.selectors = {
-      aside: asideSelector,
-      content: contentSelector,
-    };
-
-    this.elements = {
-      aside: null,
-      asideChild: null,
-      content: null,
-    };
-
-    this.topOffset = topOffset || 0;
+function GSAside(asideSelector, contentSelector, topOffset) {
+  if (!asideSelector || !contentSelector) {
+    throw new Error('Both selectors cannot be null.');
   }
 
-  GSAside.prototype.init = function() {
-    this.elements.aside = document.querySelector(this.selectors.aside);
-    if (!this.elements.aside) {
-      throw new Error(
-          'The aside with selector \'' + this.selectors.aside +
-          '\' could not be found!');
-    }
-
-    var asideChildren = this.elements.aside.children;
-    if (asideChildren.length < 1) {
-      throw new Error('The provided aside element has no children.');
-    }
-    this.elements.asideChild = asideChildren[0];
-    this.elements.asideChild.style.position = 'sticky';
-    this.elements.asideChild.style.top = this.topOffset + 'px';
-
-    this.elements.content = document.querySelector(this.selectors.content);
-    if (!this.elements.content) {
-      throw new Error(
-          'The content section with selector \'' + this.selectors.content +
-          '\' could not be found!');
-    }
-
-    this.updateAsideHeight();
+  this.selectors = {
+    aside: asideSelector,
+    content: contentSelector,
   };
 
-  GSAside.prototype.updateAsideHeight = function() {
-    var targetHeight = this.elements.content.clientHeight;
-    this.elements.aside.style.height = targetHeight + 'px';
+  this.elements = {
+    aside: null,
+    asideChild: null,
+    content: null,
   };
-})();
+
+  this.topOffset = topOffset || 0;
+}
+
+GSAside.prototype.init = function() {
+  this.elements.aside = document.querySelector(this.selectors.aside);
+  if (!this.elements.aside) {
+    throw new Error(
+        'The aside with selector \'' + this.selectors.aside +
+        '\' could not be found!');
+  }
+
+  var asideChildren = this.elements.aside.children;
+  if (asideChildren.length < 1) {
+    throw new Error('The provided aside element has no children.');
+  }
+  this.elements.asideChild = asideChildren[0];
+  this.elements.asideChild.style.position = 'sticky';
+  this.elements.asideChild.style.top = this.topOffset + 'px';
+
+  this.elements.content = document.querySelector(this.selectors.content);
+  if (!this.elements.content) {
+    throw new Error(
+        'The content section with selector \'' + this.selectors.content +
+        '\' could not be found!');
+  }
+
+  this.updateAsideHeight();
+};
+
+GSAside.prototype.updateAsideHeight = function() {
+  var targetHeight = this.elements.content.clientHeight;
+  this.elements.aside.style.height = targetHeight + 'px';
+};
