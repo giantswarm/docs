a61a801d5bf9da5523bbad249627877603dcb911
axbarsan <mnlk123@yahoo.com>
2020-05-13 13:34:24 +0200

    Only select the first 'n' levels of list items

--- src/static/js/aside.js
+++ src/static/js/aside.js
@@ -1,12 +1,13 @@
 (function() {
   function GSAside(asideSelector, contentSelector, topOffset) {
     if (!asideSelector || !contentSelector) {
-      throw new Error("Both selectors cannot be null.");
+      throw new Error('Both selectors cannot be null.');
     }
 
     this.selectors = {
       aside: asideSelector,
       content: contentSelector,
+      mainList: 'nav > ul',
     };
 
     this.elements = {
@@ -20,7 +21,8 @@
     this.topOffset = topOffset || 0;
     this.observer = null;
     this.activeLink = null;
-    this.activeClassName = "active";
+    this.activeClassName = 'active';
+    this.numLevelsToTrack = 2;
   }
 
   GSAside.prototype.throttle = function(func, limit) {
@@ -40,23 +42,23 @@
     this.elements.aside = document.querySelector(this.selectors.aside);
     if (!this.elements.aside) {
       throw new Error(
-          "The aside with selector '" + this.selectors.aside +
-          "' could not be found!");
+          'The aside with selector \'' + this.selectors.aside +
+          '\' could not be found!');
     }
 
     var asideChildren = this.elements.aside.children;
     if (asideChildren.length < 1) {
-      throw new Error("The provided aside element has no children.");
+      throw new Error('The provided aside element has no children.');
     }
     this.elements.asideChild = asideChildren[0];
-    this.elements.asideChild.style.position = "sticky";
-    this.elements.asideChild.style.top = this.topOffset + "px";
+    this.elements.asideChild.style.position = 'sticky';
+    this.elements.asideChild.style.top = this.topOffset + 'px';
 
     this.elements.content = document.querySelector(this.selectors.content);
     if (!this.elements.content) {
       throw new Error(
-          "The content section with selector '" + this.selectors.content +
-          "' could not be found!");
+          'The content section with selector \'' + this.selectors.content +
+          '\' could not be found!');
     }
 
     this.updateAsideHeight();
@@ -67,30 +69,56 @@
 
   GSAside.prototype.updateAsideHeight = function() {
     var targetHeight = this.elements.content.scrollHeight;
-    this.elements.aside.style.height = targetHeight + "px";
+    this.elements.aside.style.height = targetHeight + 'px';
   };
 
   GSAside.prototype.registerEventListeners = function() {
     var updateAsideHeight = this.throttle(this.updateAsideHeight.bind(this),
         150);
-    window.addEventListener("resize", updateAsideHeight);
+    window.addEventListener('resize', updateAsideHeight);
+  };
+
+  GSAside.prototype.generateLinksSelector = function() {
+    var selector = '';
+    for (i = 0; i < this.numLevelsToTrack; i++) {
+      var currentSelector = this.selectors.mainList;
+
+      for (var j = 0; j < i; j++) {
+        currentSelector += ' ';
+        currentSelector += '> li > ul';
+      }
+
+      currentSelector += ' ';
+      currentSelector += '> li > a';
+
+      if (selector.length > 0) {
+        selector += ', ';
+      }
+
+      selector += currentSelector;
+    }
+
+    return selector;
   };
 
   GSAside.prototype.gatherLinksAndHeaders = function() {
+    var i = 0;
+    var selector = this.generateLinksSelector();
+
     this.elements.links = [].slice.call(
-        this.elements.asideChild.querySelectorAll("a"), 0);
+        this.elements.asideChild.querySelectorAll(selector), 0);
 
     this.elements.headers = new Array(this.elements.links.length);
 
-    for (var i = 0; i < this.elements.links.length; i++) {
-      var selector = this.elements.links[i].href.split("#")[1];
+    for (i = 0; i < this.elements.links.length; i++) {
+      var selector = this.elements.links[i].href.split('#')[1];
       this.elements.headers[i] = document.getElementById(selector);
     }
   };
 
   GSAside.prototype.registerScrollObserver = function() {
     var options = {
-      rootMargin: "0px",
+      rootMargin: '0px',
       threshold: 1.0,
     };
 
@@ -109,7 +137,7 @@
   GSAside.prototype.handleScrollObserver = function(entries) {
     for (var i = 0; i < entries.length; i++) {
       var entry = entries[i];
-      var href = "#" + entry.target.getAttribute("id");
+      var href = '#' + entry.target.getAttribute('id');
 
       var correspondingLink = null;
       for (var j = 0; j < this.elements.links.length; j++) {
@@ -145,7 +173,7 @@
       link.classList.add(this.activeClassName);
       this.activeLink = link;
     }
-  }
+  };
 
   window.GSAside = GSAside;
 })();
