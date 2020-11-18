b9520135a154582ae00d0a0e799e4606f6c58776
Marian Steinbach <marian@giantswarm.io>
2020-11-18 12:29:08 +0100

    Refine automatic linking of PRs (#640)

--- scripts/aggregate-changelogs/test_script.py
+++ scripts/aggregate-changelogs/test_script.py
@@ -2,7 +2,12 @@ from script import link_pull_requests
 
 if __name__ == "__main__":
 
-    s = "Adds something (#123) to fix #45"
-    result = link_pull_requests(s, "org/repo")
+    s1 = "Adds something (#123) to fix #45"
+    result = link_pull_requests(s1, "org/repo")
     print(result)
-    assert result == "Adds something ([#123](https://github.com/org/repo/pulls/123)) to fix [#45](https://github.com/org/repo/pulls/45)"
+    assert result == "Adds something ([#123](https://github.com/org/repo/pull/123)) to fix [#45](https://github.com/org/repo/pull/45)"
+
+    s2 = "Blah blah [otherrepo#7](https://github.com/giantswarm/otherrepo/pull/7)"
+    result = link_pull_requests(s2, "org/thisrepo")
+    print(result)
+    assert result == s2
