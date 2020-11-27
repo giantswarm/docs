d5c00a2c10d4e0ce7a273eabd9dfd47f7d9dc124
Marian Steinbach <marian@giantswarm.io>
2020-11-27 12:13:18 +0100

    Improve CRD script

--- scripts/update-crd-reference/main.sh
+++ scripts/update-crd-reference/main.sh
@@ -2,8 +2,14 @@
 
 CRD_DOCS_GENERATOR_VERSION=0.1.2
 
+DESTINATION=src/content/reference/cp-k8s-api
+
+# Clear output folder
+find ${DESTINATION} -type f -not -name "_index.md" | xargs rm
+
+# Generate new content
 docker run \
-    -v ${PWD}/src/content/reference/cp-k8s-api:/opt/crd-docs-generator/output \
+    -v ${PWD}/${DESTINATION}:/opt/crd-docs-generator/output \
     -v ${PWD}/scripts/update-crd-reference:/opt/crd-docs-generator/config \
     quay.io/giantswarm/crd-docs-generator:${CRD_DOCS_GENERATOR_VERSION} \
         --config /opt/crd-docs-generator/config/config.yaml
