1d3266a78d2c5c9486d112da13fc194a9136bef7
Oliver Ponder <oliverponder@giantswarm.io>
2020-12-07 21:30:29 +0800

    Add main site header (#677)
    * Update to latest code.
    
    * Add command to reproducibly fetch header and footer code from the main site.
    
    * Commit latest gs_header and gs_footer as fetched using the make target.
    
    * Make the header's interactive features work again.
    
    * Add mobile menu script and make sure things are looking and working ok.
    
    * Update src/assets/scripts/gs_menu.js
    
    Co-authored-by: Adrian Barsan <mnlk123@yahoo.com>
    
    * Update src/assets/scripts/gs_menu.js
    
    Co-authored-by: Adrian Barsan <mnlk123@yahoo.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Adrian Barsan <mnlk123@yahoo.com>
    
    * Add a bit in docs about how the header and footer works.
    
    * Fix menu functionality.
    
    * Update src/assets/scripts/gs_menu.js
    
    Co-authored-by: Marian Steinbach <marian@giantswarm.io>
    
    Co-authored-by: Adrian Barsan <mnlk123@yahoo.com>
    Co-authored-by: Marian Steinbach <marian@giantswarm.io>

---
+++ src/assets/scripts/gs_menu.js
@@ -0,0 +1,68 @@
+// This recreates the behaviour of the Giant Swarm menu without copying over all
+// of that site's javascript.
+
+var headerLinks = document.querySelectorAll(".hs-menu-depth-1 > a");
+for (var i = 0; i < headerLinks.length; i++) {
+  headerLinks[i].addEventListener('click', toggleMenu, false);
+}
+
+function toggleMenu(e) {
+  e.preventDefault();
+
+  e.currentTarget.parentElement.classList.toggle("menu-expanded");
+
+  var wrapperContainer = e.currentTarget.parentElement.querySelector('.hs-menu-children-wrapper-container');
+
+  if (wrapperContainer.style.display === "none" || !wrapperContainer.style.display) {
+    closeAllMenus();
+    wrapperContainer.style.display = "block";
+  } else {
+    wrapperContainer.style.display = "none";
+  }
+}
+
+function closeAllMenus() {
+  for (var i = 0; i < headerLinks.length; i++) {
+    var wrapperContainer = headerLinks[i].parentElement.querySelector('.hs-menu-children-wrapper-container');
+
+    if (wrapperContainer) {
+      wrapperContainer.style.display = "none";
+    }
+  }
+}
+
+// Recreate open/close behaviour of the mobile menu.
+var mobileMenuTrigger = document.querySelector('.menu-trigger__container');
+var mobileMenu = document.querySelector('.mobile-menu');
+mobileMenuTrigger.addEventListener('click', function() {
+  mobileMenuTrigger.classList.toggle('open');
+  mobileMenu.classList.toggle('open');
+});
+
+function clickHandler(link) {
+  var l = link;
+  return function(e) {
+    e.preventDefault();
+    var ex = l.getAttribute('aria-expanded');
+    if (ex === 'true') {
+      l.setAttribute('aria-expanded', 'false')
+    } else {
+      l.setAttribute('aria-expanded', 'true')
+    }
+
+    var itemContainer = l.nextElementSibling.querySelector('.hs-menu-children-wrapper');
+    if (itemContainer.style.display === 'none') {
+      itemContainer.style.display = 'block';
+    } else {
+      itemContainer.style.display = 'none';
+    }
+  }
+}
+
+// Recreate open/close behaviour of individual menu items in the mobile menu.
+var mobileMenuLinks = document.querySelectorAll('.mobile-menu .hs-menu-depth-1 > a');
+for (var i = 0; i < mobileMenuLinks.length; i++) {
+  var link = mobileMenuLinks[i];
+
+  link.addEventListener('click', clickHandler(link));
+}
