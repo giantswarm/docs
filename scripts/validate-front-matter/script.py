a7c1942efe571341a906d39deca0dac50806cd49
Marian Steinbach <marian@giantswarm.io>
2021-06-23 16:16:22 +0200

    Add another new line

--- scripts/validate-front-matter/script.py
+++ scripts/validate-front-matter/script.py
@@ -191,7 +191,7 @@ def dump_result(rdict):
             print(f'  - {item}')
 
     if fail:
-        print("Some problems found.")
+        print("\nSome problems found.")
         print(f"Please fix at least those marked with {severity('FAIL')}.", end="\n\n")
         sys.exit(1)
 
