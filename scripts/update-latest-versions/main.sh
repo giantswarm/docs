679391885d2d48687662294eddc489b02d62b025
Marian Steinbach <marian@giantswarm.io>
2021-07-02 12:26:45 +0200

    Simplify command

--- scripts/update-latest-versions/main.sh
+++ scripts/update-latest-versions/main.sh
@@ -10,10 +10,10 @@
 set -e
 
 docker run --rm --entrypoint /bin/sh quay.io/giantswarm/docs-scriptrunner \
-    -c "curl --silent https://api.github.com/repos/giantswarm/gsctl/releases/latest|jq -r .tag_name|tr -d '\n'" \
+    -c "curl --silent https://api.github.com/repos/giantswarm/gsctl/releases/latest|jq -j .tag_name" \
     > src/layouts/shortcodes/gsctl_version.html
 
 # For kubectl-gs, we have to cut the leading 'v' from the tag
 docker run --rm --entrypoint /bin/sh quay.io/giantswarm/docs-scriptrunner \
-    -c "curl --silent https://api.github.com/repos/giantswarm/kubectl-gs/releases/latest|jq -r .tag_name | cut -c 2- | tr -d '\n'" \
+    -c "curl --silent https://api.github.com/repos/giantswarm/kubectl-gs/releases/latest|jq -j .tag_name | cut -c 2- | tr -d '\n'" \
     > src/layouts/shortcodes/kubectl_gs_version.html
