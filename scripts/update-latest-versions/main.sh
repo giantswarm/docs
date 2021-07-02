f3652859095b8259609bee9f1657d28c2f464e26
Marian Steinbach <marian@giantswarm.io>
2021-07-02 11:15:42 +0200

    Remove leading v from kubectl-gs version shortcode

--- scripts/update-latest-versions/main.sh
+++ scripts/update-latest-versions/main.sh
@@ -3,6 +3,7 @@
 # This script updates the latest version numbers of
 #
 # - gsctl
+# - kubectl-gs
 #
 # displayed in docs.
 
@@ -12,6 +13,7 @@ docker run --rm --entrypoint /bin/sh quay.io/giantswarm/docs-scriptrunner \
     -c "curl --silent https://api.github.com/repos/giantswarm/gsctl/releases/latest|jq -r .tag_name|tr -d '\n'" \
     > src/layouts/shortcodes/gsctl_version.html
 
+# For kubectl-gs, we have to cut the leading 'v' from the tag
 docker run --rm --entrypoint /bin/sh quay.io/giantswarm/docs-scriptrunner \
-    -c "curl --silent https://api.github.com/repos/giantswarm/kubectl-gs/releases/latest|jq -r .tag_name|tr -d '\n'" \
+    -c "curl --silent https://api.github.com/repos/giantswarm/kubectl-gs/releases/latest|jq -r .tag_name | cut -c 2- | tr -d '\n'" \
     > src/layouts/shortcodes/kubectl_gs_version.html
