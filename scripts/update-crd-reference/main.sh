f48e79909e0792c21fcb375465428446421b88ef
Marian Steinbach <marian@giantswarm.io>
2021-07-19 14:25:21 +0200

    Set apiextensions and crd-docs-generator version

--- scripts/update-crd-reference/main.sh
+++ scripts/update-crd-reference/main.sh
@@ -2,18 +2,14 @@
 
 set -e
 
-# TODO: set release version
-CRD_DOCS_GENERATOR_VERSION=0.6.1-d98f2e890b9274ca2ed3a3dddbacfe7344942667
+CRD_DOCS_GENERATOR_VERSION=0.7.0
 DESTINATION=src/content/ui-api/management-api/crd
 
 # Clear output folder
 find ${DESTINATION} -type f -not -name "_index.md" | xargs rm
 
 # Determine apiextensions version
-#VERSION=$(docker run --rm --entrypoint /bin/sh quay.io/giantswarm/docs-scriptrunner -c "curl --silent https://api.github.com/repos/giantswarm/apiextensions/releases/latest|jq -r .tag_name|tr -d '\n'")
-
-# TODO: revert this
-VERSION=add-docs-metadata
+VERSION=$(docker run --rm --entrypoint /bin/sh quay.io/giantswarm/docs-scriptrunner -c "curl --silent https://api.github.com/repos/giantswarm/apiextensions/releases/latest|jq -r .tag_name|tr -d '\n'")
 
 echo "Version: ${VERSION}"
 
