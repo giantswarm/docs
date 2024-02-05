dc30fb2ce6473c5d87e85e6333bfdec1c9cdd70e
pipo02mix <fernando@giantswarm.io>
2024-02-05 13:44:22 +0100

    Last changes

--- scripts/update-helm-chart-reference/main.sh
+++ scripts/update-helm-chart-reference/main.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-HELM_CHART_DOCS_GENERATOR_VERSION=0.0.0-8dec37b630cd548a9182fa0d21486f4c45a7c828
+HELM_CHART_DOCS_GENERATOR_VERSION=0.1.0
 DESTINATION=src/content/use-the-api/management-api/cluster-apps
 
 # Clear output folder
@@ -12,5 +12,5 @@ find ${DESTINATION} -type f -not -name "_index.md" | xargs -I '{}' rm '{}'
 docker run --rm \
     -v ${PWD}/${DESTINATION}:/opt/helm-chart-docs-generator/output \
     -v ${PWD}/scripts/update-helm-chart-reference:/opt/helm-chart-docs-generator/config \
-    giantswarm/helm-chart-docs-generator:0.0.1 \
+    gsoci.azurecr.io/giantswarm/helm-chart-docs-generator:${HELM_CHART_DOCS_GENERATOR_VERSION} \
         --config /opt/helm-chart-docs-generator/config/config.yaml
