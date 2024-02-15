c436e467e43d72a0c316fe8a2fa3d3e40a1b855b
Fernando Ripoll <fernando@giantswarm.io>
2024-02-15 11:32:41 +0100

    Missed to update the script container image (#2121)
    * Fix naming
    
    * Last changes
    
    * Work on the template
    
    * Fix version of generator
    
    * Include all cluster charts
    
    * Include all apps
    
    * Generate the docs automatically
    
    * Set proper image for building it

--- scripts/update-helm-chart-reference/main.sh
+++ scripts/update-helm-chart-reference/main.sh
@@ -7,13 +7,11 @@ DESTINATION=src/content/use-the-api/management-api/cluster-apps
 find ${DESTINATION} -type f -not -name "_index.md" | xargs -I '{}' rm '{}'
 
 # Generate new content
-# docker run --rm \
-#     -v ${PWD}/${DESTINATION}:/opt/helm-chart-docs-generator/output \
-#     -v ${PWD}/scripts/update-helm-chart-reference:/opt/helm-chart-docs-generator/config \
-#     gsoci.azurecr.io/giantswarm/helm-chart-docs-generator:${HELM_CHART_DOCS_GENERATOR_VERSION} \
-#         --config /opt/helm-chart-docs-generator/config/config.yaml
-
-docker run --rm -v $PWD/src/content/use-the-api/management-api/cluster-apps:/opt/helm-chart-docs-generator/output -v $PWD/scripts/update-helm-chart-reference:/opt/helm-chart-docs-generator/config giantswarm/helm-chart-docs-generator:0.0.1 --config /opt/helm-chart-docs-generator/config/config.yaml
+docker run --rm \
+    -v ${PWD}/${DESTINATION}:/opt/helm-chart-docs-generator/output \
+    -v ${PWD}/scripts/update-helm-chart-reference:/opt/helm-chart-docs-generator/config \
+    gsoci.azurecr.io/giantswarm/helm-chart-docs-generator:${HELM_CHART_DOCS_GENERATOR_VERSION} \
+        --config /opt/helm-chart-docs-generator/config/config.yaml
 
 for file in ${DESTINATION}/*.md; do
   if grep -q '\--&gt;' $file; then
