9bb0c40754539d34d1cec39361a6b7f3ea894336
Marian Steinbach <marian@giantswarm.io>
2026-01-21 11:21:38 +0100

    Turn relative links in release notes / changelog entries into absolute

--- scripts/aggregate-changelogs/test_script.py
+++ scripts/aggregate-changelogs/test_script.py
@@ -1,4 +1,4 @@
-from script import link_pull_requests
+from script import link_pull_requests, resolve_relative_links
 
 if __name__ == "__main__":
 
@@ -11,3 +11,32 @@ if __name__ == "__main__":
     result = link_pull_requests(s2, "org/thisrepo")
     print(result)
     assert result == s2
+
+    # Test resolve_relative_links
+    print("\nTesting resolve_relative_links:")
+
+    # Test relative link with ./ and 'main' branch
+    s3 = "See [changelog](./docs/releases/v0.95.0-changelog.md) for more info."
+    result = resolve_relative_links(s3, "giantswarm/backstage", "main")
+    print(result)
+    assert result == "See [changelog](https://github.com/giantswarm/backstage/blob/main/docs/releases/v0.95.0-changelog.md) for more info."
+
+    # Test relative link without ./ and 'master' branch
+    s4 = "Check the [README](README.md)."
+    result = resolve_relative_links(s4, "giantswarm/oldrepo", "master")
+    print(result)
+    assert result == "Check the [README](https://github.com/giantswarm/oldrepo/blob/master/README.md)."
+
+    # Test that absolute URLs are not modified
+    s5 = "See [docs](https://example.com/docs) and [guide](./guide.md)."
+    result = resolve_relative_links(s5, "giantswarm/backstage", "main")
+    print(result)
+    assert result == "See [docs](https://example.com/docs) and [guide](https://github.com/giantswarm/backstage/blob/main/guide.md)."
+
+    # Test that anchors are not modified
+    s6 = "Jump to [section](#section) or read [more](./more.md)."
+    result = resolve_relative_links(s6, "giantswarm/backstage", "main")
+    print(result)
+    assert result == "Jump to [section](#section) or read [more](https://github.com/giantswarm/backstage/blob/main/more.md)."
+
+    print("\nAll tests passed!")
